462acaaaf5dd43d5ac7e5daef2992b8f891b749e
Kim-soung-won <rlatmddnjs0103@naver.com>
2024-11-11 10:55:18 +0900

    env 추가

---
+++ src/shared/lib/react-router/config.ts
@@ -0,0 +1,3 @@
+export const pathKeys = {
+  root: import.meta.env.VITE_BASE_SERVER_URL,
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslint from "vite-plugin-eslint";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    eslint({
-      overrideConfigFile: ".eslintrc.cjs",
-      cache: false,
-      fix: true,
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return defineConfig({
+    ...(mode !== "test" && {
+      plugins: [
+        react(),
+        eslint({
+          overrideConfigFile: ".eslintrc.cjs",
+          cache: false,
+          fix: true,
+        }),
+      ],
     }),
-  ],
-})
+    server: {
+      proxy: {
+        "/api": {
+          target: process.env.VITE_BASE_SERVER_URL,
+          changeOrigin: true,
+          secure: false,
+        }
+      }
+    }
+  })
+}
